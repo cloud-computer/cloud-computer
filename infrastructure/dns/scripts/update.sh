6e6cab49b52b5e9d7cf78ccf627295b5b2a5a19d
Jackson Delahunt <jackson@stemn.com>
2019-05-05 08:21:48 +1000

    update

---
+++ infrastructure/dns/scripts/update.sh
@@ -0,0 +1,5 @@
+# Export cloud computer shell environment
+eval "$(yarn --cwd ../cloud-computer environment)"
+
+# Add a mapping to the terraform host
+yarn set-host $CLOUD_COMPUTER_HOST_DNS $(yarn --cwd ../terraform ip --no-cache)
