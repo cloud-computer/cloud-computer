22d9f5827c034d7f82d37540e62ecc3c04b64c5b
Jackson Delahunt <jackson@stemn.com>
2019-05-05 08:57:29 +1000

    update

--- infrastructure/dns/scripts/update.sh
+++ infrastructure/dns/scripts/update.sh
@@ -1,5 +1,5 @@
 # Export cloud computer shell environment
 eval "$(yarn --cwd ../cloud-computer environment)"
 
-# Add a mapping to the terraform host
-yarn set-host $CLOUD_COMPUTER_HOST_DNS $(yarn --cwd ../terraform ip)
+# Add a wildcard dns entry for the terraform host
+yarn set-host *.$CLOUD_COMPUTER_HOST_DNS $(yarn --cwd ../terraform ip)
