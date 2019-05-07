61c303f31aa869e30042077db02fe1d14bb888f9
Jackson Delahunt <jackson@stemn.com>
2019-05-07 17:09:50 +1000

    Revert "no-cers"
    This reverts commit 4699c660cdf9b8af1ad5bb3b41fe08c6e3c03e65.

--- infrastructure/docker/scripts/docker.sh
+++ infrastructure/docker/scripts/docker.sh
@@ -4,15 +4,21 @@ eval "$(yarn --cwd ../cloud-computer environment)"
 if [ "$DOCKER_HOST" = "localhost" ]; then
 
   # Always connect to localhost via the unix socket
+  DOCKER_CERT_PATH=
   DOCKER_HOST=unix:///var/run/docker.sock
+  DOCKER_TLS_VERIFY=
 
 elif [ -z "$DOCKER_HOST" ]; then
 
   # Use tls when connecting to a remote host
+  DOCKER_CERT_PATH=$PWD
   DOCKER_HOST=docker.$CLOUD_COMPUTER_HOST_DNS:443
+  DOCKER_TLS_VERIFY=1
 
 fi
 
+export DOCKER_CERT_PATH=$DOCKER_CERT_PATH
 export DOCKER_HOST=$DOCKER_HOST
+export DOCKER_TLS_VERIFY=$DOCKER_TLS_VERIFY
 
 docker --tls "$@"
