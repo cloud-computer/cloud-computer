9ed2b29ca72eaf4a82d51a73dd02c042575e068b
Jackson Delahunt <jackson@stemn.com>
2019-05-07 17:12:32 +1000

    tls

--- infrastructure/docker/scripts/docker.sh
+++ infrastructure/docker/scripts/docker.sh
@@ -4,7 +4,6 @@ eval "$(yarn --cwd ../cloud-computer environment)"
 if [ "$DOCKER_HOST" = "localhost" ]; then
 
   # Always connect to localhost via the unix socket
-  DOCKER_CERT_PATH=
   DOCKER_HOST=unix:///var/run/docker.sock
   DOCKER_TLS_VERIFY=
 
@@ -12,9 +11,11 @@ elif [ -z "$DOCKER_HOST" ]; then
 
   # Use tls when connecting to a remote host
   DOCKER_HOST=docker.$CLOUD_COMPUTER_HOST_DNS:443
+  DOCKER_TLS_VERIFY=1
 
 fi
 
 export DOCKER_HOST=$DOCKER_HOST
+export DOCKER_TLS_VERIFY=$DOCKER_TLS_VERIFY
 
-docker --tls "$@"
+docker "$@"
