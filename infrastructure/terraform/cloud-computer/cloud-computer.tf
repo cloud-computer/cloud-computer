a4d817cdceb266eeb5b787b3fa01d97e4976336e
Jackson Delahunt <jackson@stemn.com>
2019-05-09 07:06:02 +1000

    as-root

--- infrastructure/terraform/cloud-computer/cloud-computer.tf
+++ infrastructure/terraform/cloud-computer/cloud-computer.tf
@@ -101,10 +101,10 @@ resource "google_compute_instance" "cloud-computer" {
       "export CLOUD_COMPUTER_CLOUD_PROVIDER_CREDENTIALS='${file("${var.cloud_provider_credentials_path}")}'",
       "export CLOUD_COMPUTER_HOST_ID=${var.CLOUD_COMPUTER_HOST_ID}",
 
-      "printf %s '$CLOUD_COMPUTER_CLOUD_PROVIDER_CREDENTIALS'",
+      "printf %s $CLOUD_COMPUTER_CLOUD_PROVIDER_CREDENTIALS",
 
       "# Alias docker run",
-      "alias docker_run=\"docker run --env CLOUD_COMPUTER_HOST_ID --env CLOUD_COMPUTER_CLOUD_PROVIDER_CREDENTIALS --env DOCKER_HOST=localhost --interactive --rm --tty --volume $CLOUD_COMPUTER_BACKEND_VOLUME:$CLOUD_COMPUTER_BACKEND --volume /var/run/docker.sock:/var/run/docker.sock --workdir $CLOUD_COMPUTER_BACKEND ${var.CLOUD_COMPUTER_REGISTRY}/cloud-computer\"",
+      "alias docker_run=\"docker run --env CLOUD_COMPUTER_HOST_ID --env CLOUD_COMPUTER_CLOUD_PROVIDER_CREDENTIALS --env DOCKER_HOST=localhost --interactive --rm --tty --user root --volume $CLOUD_COMPUTER_BACKEND_VOLUME:$CLOUD_COMPUTER_BACKEND --volume /var/run/docker.sock:/var/run/docker.sock --workdir $CLOUD_COMPUTER_BACKEND ${var.CLOUD_COMPUTER_REGISTRY}/cloud-computer\"",
 
       "# Clone the cloud computer",
       "docker_run git clone --branch master --depth 1 --quiet --single-branch https://github.com/cloud-computer/cloud-computer $CLOUD_COMPUTER_BACKEND",
