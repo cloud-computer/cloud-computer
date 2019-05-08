9fc79064f3398ea9cf57354c9f38f841f1b2ee0a
Jackson Delahunt <jackson@stemn.com>
2019-05-08 12:29:24 +1000

    script

--- infrastructure/terraform/cloud-computer/cloud-computer.tf
+++ infrastructure/terraform/cloud-computer/cloud-computer.tf
@@ -88,30 +88,12 @@ resource "google_compute_instance" "cloud-computer" {
 
   provisioner "remote-exec" {
     connection {
+      agent = false
+      private_key = "${tls_private_key.cloud-computer.private_key_pem}"
+      script_path = "/root/bootstrap.sh"
       type = "ssh"
       user = "core"
-      private_key = "${tls_private_key.cloud-computer.private_key_pem}"
-      agent = false
     }
-
-    inline = [
-      "# Set cloud computer environment",
-      "export CLOUD_COMPUTER_BACKEND=${var.CLOUD_COMPUTER_BACKEND}",
-      "export CLOUD_COMPUTER_BACKEND_VOLUME=${var.CLOUD_COMPUTER_BACKEND_VOLUME}",
-      "export CLOUD_COMPUTER_HOST_ID=${var.CLOUD_COMPUTER_HOST_ID}",
-
-      "# Target the local docker socket",
-      "export DOCKER_HOST=unix:///var/run/docker.sock",
-
-      "# Alias docker run",
-      "alias docker_run=\"docker run --env CLOUD_COMPUTER_HOST_ID --env DOCKER_HOST --rm --volume $CLOUD_COMPUTER_BACKEND_VOLUME:$CLOUD_COMPUTER_BACKEND --volume /var/run/docker.sock:/var/run/docker.sock --workdir $CLOUD_COMPUTER_BACKEND ${var.CLOUD_COMPUTER_REPOSITORY}/bootstrap\"",
-
-      "# Clone the cloud computer",
-      "docker_run git clone --branch master --depth 1 --quiet --single-branch https://github.com/cloud-computer/cloud-computer $CLOUD_COMPUTER_BACKEND",
-
-      "# Expose the docker socket",
-      "docker_run yarn --cwd infrastructure/docker-compose up:docker",
-      "docker_run yarn --cwd infrastructure/docker-compose up:traefik",
-    ]
+    script = "${path.module}/bootstrap.sh"
   }
 }
