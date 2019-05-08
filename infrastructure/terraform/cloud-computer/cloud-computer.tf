8d42bfb09697b776da77009d28825e67148f1e59
Jackson Delahunt <jackson@stemn.com>
2019-05-08 15:33:00 +1000

    as-root

--- infrastructure/terraform/cloud-computer/cloud-computer.tf
+++ infrastructure/terraform/cloud-computer/cloud-computer.tf
@@ -77,7 +77,7 @@ resource "google_compute_instance" "cloud-computer" {
   }
 
   metadata {
-    ssh-keys = "ubuntu:${tls_private_key.cloud-computer.public_key_openssh}"
+    ssh-keys = "root:${tls_private_key.cloud-computer.public_key_openssh}"
   }
 
   network_interface {
@@ -91,7 +91,7 @@ resource "google_compute_instance" "cloud-computer" {
       agent = false
       private_key = "${tls_private_key.cloud-computer.private_key_pem}"
       type = "ssh"
-      user = "ubuntu"
+      user = "root"
     }
 
     inline = [
@@ -107,7 +107,7 @@ resource "google_compute_instance" "cloud-computer" {
       "export DOCKER_HOST=unix:///var/run/docker.sock",
 
       "# Alias docker run",
-      "alias docker_run=\"docker run --env CLOUD_COMPUTER_HOST_ID --env DOCKER_HOST --interactive --rm --tty --volume $CLOUD_COMPUTER_BACKEND_VOLUME:$CLOUD_COMPUTER_BACKEND --volume /var/run/docker.sock:/var/run/docker.sock --workdir $CLOUD_COMPUTER_BACKEND ${var.CLOUD_COMPUTER_REPOSITORY}/bootstrap\"",
+      "alias docker_run=\"docker run --env CLOUD_COMPUTER_HOST_ID --env DOCKER_HOST --interactive --rm --tty --user ubuntu --volume $CLOUD_COMPUTER_BACKEND_VOLUME:$CLOUD_COMPUTER_BACKEND --volume /var/run/docker.sock:/var/run/docker.sock --workdir $CLOUD_COMPUTER_BACKEND ${var.CLOUD_COMPUTER_REPOSITORY}/bootstrap\"",
 
       "# Clone the cloud computer",
       "docker_run git clone --branch master --depth 1 --quiet --single-branch https://github.com/cloud-computer/cloud-computer $CLOUD_COMPUTER_BACKEND",
