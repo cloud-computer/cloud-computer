a0597f98397677a7fec426d2941fe5749f643937
Jackson Delahunt <jackson@stemn.com>
2019-05-09 17:54:26 +1000

    fixes

--- scripts/links.sh
+++ scripts/links.sh
@@ -1,8 +1,8 @@
-# Export stemn shell environment
-eval "$(yarn --cwd infrastructure/development-environment environment)"
+# Export cloud computer shell environment
+eval "$(yarn --cwd infrastructure/cloud-computer environment)"
 
 # Print links for click-to-open access
 echo
 yarn apps | \
   tr \  \\n | \
-  xargs -n 1 -I% echo https://%-$CLOUD_COMPUTER_HOST_DNS
+  xargs -n 1 -I% echo https://%.$CLOUD_COMPUTER_HOST_DNS
--- scripts/open.sh
+++ scripts/open.sh
@@ -1,8 +1,8 @@
-# Export stemn shell environment
-eval "$(yarn --cwd infrastructure/development-environment environment)"
+# Export cloud computer shell environment
+eval "$(yarn --cwd infrastructure/cloud-computer environment)"
 
 # Prompt which app to open in app mode
 yarn apps | \
   tr \  \\n | \
   yarn ipt | \
-  xargs -n 1 -I% nohup google-chrome --app=https://%-$CLOUD_COMPUTER_HOST_DNS >/dev/null 2>&1
+  xargs -n 1 -I% nohup google-chrome --app=https://%.$CLOUD_COMPUTER_HOST_DNS >/dev/null 2>&1
