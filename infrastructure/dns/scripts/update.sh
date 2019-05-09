d8ab1691c8fff0e178632d588970436f4f6ce558
Jackson Delahunt <jackson@stemn.com>
2019-05-09 11:27:26 +1000

    workdir-script

--- infrastructure/dns/scripts/update.sh
+++ infrastructure/dns/scripts/update.sh
@@ -3,5 +3,3 @@ eval "$(yarn --cwd ../cloud-computer environment)"
 
 # Point cloud computer dns to terraform host
 yarn set-record *.$CLOUD_COMPUTER_HOST_DNS $(yarn --cwd ../terraform ip)
-
-exit 3
---
+++ infrastructure/terraform/scripts/terraform-workdir.sh
@@ -0,0 +1,28 @@
+# Export cloud computer shell environment
+eval "$(yarn --cwd ../cloud-computer environment)"
+
+# Point shell context to the local docker host
+export DOCKER_HOST=localhost
+
+# Specialised terraform executable for commands that require workdir to be the terraform module root
+yarn --cwd ../docker docker run \
+  --env GOOGLE_APPLICATION_CREDENTIALS=$CLOUD_COMPUTER_CREDENTIALS/cloud-provider.json \
+  --env TF_CLI_ARGS_destroy="-auto-approve -lock=false" \
+  --env TF_CLI_ARGS_taint="-lock=false" \
+  --env TF_DATA_DIR=$CLOUD_COMPUTER_TERRAFORM \
+  --env TF_IN_AUTOMATION=true \
+  --env TF_VAR_CLOUD_COMPUTER_CLOUD_PROVIDER_PROJECT=$(yarn --cwd ../credentials project) \
+  --env TF_VAR_CLOUD_COMPUTER_HOST_ID=$CLOUD_COMPUTER_HOST_ID \
+  --env TF_VAR_CLOUD_COMPUTER_HOST_NAME=$CLOUD_COMPUTER_HOST_NAME \
+  --env TF_VAR_CLOUD_COMPUTER_HOST_USER=$CLOUD_COMPUTER_HOST_USER \
+  --env TF_VAR_CLOUD_COMPUTER_IMAGE=$CLOUD_COMPUTER_IMAGE \
+  --env TF_VAR_CLOUD_COMPUTER_REGISTRY=$CLOUD_COMPUTER_REGISTRY \
+  --env TF_VAR_CLOUD_COMPUTER_REPOSITORY=$CLOUD_COMPUTER_REPOSITORY \
+  --env TF_VAR_CLOUD_COMPUTER_REPOSITORY_VOLUME=$CLOUD_COMPUTER_REPOSITORY_VOLUME \
+  --rm \
+  --volume $CLOUD_COMPUTER_CREDENTIALS_VOLUME:$CLOUD_COMPUTER_CREDENTIALS \
+  --volume $CLOUD_COMPUTER_REPOSITORY_VOLUME:$CLOUD_COMPUTER_REPOSITORY \
+  --volume $CLOUD_COMPUTER_TERRAFORM_VOLUME:$CLOUD_COMPUTER_TERRAFORM \
+  --workdir $(yarn workdir) \
+  $CLOUD_COMPUTER_REGISTRY/$CLOUD_COMPUTER_IMAGE:latest \
+  terraform "$@"
