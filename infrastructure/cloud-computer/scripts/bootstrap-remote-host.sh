af7d02eecbcaf47c0ff0e82075d82d6eaed2673e
Jackson Delahunt <jackson@stemn.com>
2019-05-09 17:34:26 +1000

    no-cache-populate

--- infrastructure/cloud-computer/scripts/bootstrap-remote-host.sh
+++ infrastructure/cloud-computer/scripts/bootstrap-remote-host.sh
@@ -6,6 +6,3 @@ yarn --cwd ../docker-compose up:terminal
 
 # Bootstrap the cloud computer repository
 yarn exec:tty:terminal yarn bootstrap
-
-# Populate the cloud computer docker image build cache in the background
-yarn exec:tty:terminal yarn --cwd infrastructure/cloud-computer tmux new-session -d -s docker-build-cache-populate yarn --cwd ../containers sync:cloud-computer
