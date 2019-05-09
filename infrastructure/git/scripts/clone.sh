4f370073d4caa0ca04fe352a1b2690a2e6818615
Jackson Delahunt <jackson@stemn.com>
2019-05-09 10:30:49 +1000

    clone

--- infrastructure/git/scripts/clone.sh
+++ infrastructure/git/scripts/clone.sh
@@ -1,8 +1,17 @@
+# Export cloud computer shell environment
+eval "$(yarn --cwd ../cloud-computer environment)"
+
 # Local repository branch
 GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-# Clone cloud-computer backend repository
-yarn git clone https://github.com/cloud-computer/cloud-computer.git --branch $GIT_BRANCH --depth 1 --single-branch . 2>/dev/null
+# Clone cloud-computer backend repository (unknown issue: git clone causes root ownership of repository volume)
+yarn --cwd ../docker docker run \
+  --rm \
+  --volume $CLOUD_COMPUTER_REPOSITORY_VOLUME:$CLOUD_COMPUTER_REPOSITORY \
+  --user root \
+  --workdir $CLOUD_COMPUTER_REPOSITORY \
+  $CLOUD_COMPUTER_REGISTRY/$CLOUD_COMPUTER_IMAGE:latest \
+  zsh -c "git clone https://github.com/cloud-computer/cloud-computer.git --branch $GIT_BRANCH --depth 1 --single-branch .; chown -R cloud:cloud ." 2>/dev/null
 
 # Restore refs after shallow clone
 yarn git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
