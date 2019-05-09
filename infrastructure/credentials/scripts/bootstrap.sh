d8e818e2b62587e9b822820f6830be87f31b54b7
Jackson Delahunt <jackson@stemn.com>
2019-05-10 07:54:34 +1000

    host

--- infrastructure/credentials/scripts/bootstrap.sh
+++ infrastructure/credentials/scripts/bootstrap.sh
@@ -1,8 +1,8 @@
 # Export cloud computer shell environment
 eval "$(yarn --cwd ../cloud-computer environment)"
 
-# Extract credentials from the environment or repository
-CLOUD_COMPUTER_CLOUD_PROVIDER_CREDENTIALS=${CLOUD_COMPUTER_CLOUD_PROVIDER_CREDENTIALS-$(cat $PWD/cloud-provider.json)}
+# Point shell context to the local docker host
+export DOCKER_HOST=localhost
 
 # Create the CLOUD_COMPUTER_CREDENTIALS volume and take ownership of it
 yarn --cwd ../docker docker run \
@@ -11,6 +11,9 @@ yarn --cwd ../docker docker run \
   --volume $CLOUD_COMPUTER_CREDENTIALS_VOLUME:$CLOUD_COMPUTER_CREDENTIALS \
   $CLOUD_COMPUTER_REGISTRY/$CLOUD_COMPUTER_IMAGE:latest \
   chown -R cloud:cloud $CLOUD_COMPUTER_CREDENTIALS
+
+# Extract credentials from the environment or local disk
+CLOUD_COMPUTER_CLOUD_PROVIDER_CREDENTIALS=${CLOUD_COMPUTER_CLOUD_PROVIDER_CREDENTIALS-$(cat $PWD/cloud-provider.json)}
 
 # Copy cloud provider credentials to the CLOUD_COMPUTER_CREDENTIALS volume
 yarn --cwd ../docker docker run \
