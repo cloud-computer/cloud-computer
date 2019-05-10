36e1d46b9d7a5b3b3453d008aacf04780ec3b117
Jackson Delahunt <jackson@stemn.com>
2019-05-10 14:30:50 +1000

    Revert "logging"
    This reverts commit d0ca3bfe92d08b2f89541df1eaf993deee39d76e.

--- infrastructure/cloud-computer/scripts/bootstrap-infrastructure.sh
+++ infrastructure/cloud-computer/scripts/bootstrap-infrastructure.sh
@@ -10,27 +10,17 @@ yarn --cwd ../yarn link:global
 # Clear the shell resolve history to enable the yarn shim
 hash -r
 
-echo 1111111111111111111111111111111111111
-
 # Bootstrap credentials configuration
-yarn --cwd ../credentials bootstrap
-
-echo 2222222222222222222222222222222222222
+yarn --cwd ../credentials bootstrap &
 
 # Bootstrap dns configuration
-yarn --cwd ../dns bootstrap
-
-echo 3333333333333333333333333333333333333
+yarn --cwd ../dns bootstrap &
 
 # Bootstrap git configuration
-yarn --cwd ../git bootstrap
-
-echo 4444444444444444444444444444444444444
+yarn --cwd ../git bootstrap &
 
 # Bootstrap yarn configuration
-yarn --cwd ../yarn bootstrap
-
-echo 5555555555555555555555555555555555555
+yarn --cwd ../yarn bootstrap &
 
 # Create the CLOUD_COMPUTER_HOME volume and take ownership of it
 yarn --cwd ../docker docker run \
@@ -38,9 +28,7 @@ yarn --cwd ../docker docker run \
   --user root \
   --volume $CLOUD_COMPUTER_HOME_VOLUME:$CLOUD_COMPUTER_HOME \
   $CLOUD_COMPUTER_REGISTRY/$CLOUD_COMPUTER_IMAGE:latest \
-  chown -R cloud:cloud $CLOUD_COMPUTER_HOME
-
-echo 6666666666666666666666666666666666666
+  chown -R cloud:cloud $CLOUD_COMPUTER_HOME &
 
 # Create the CLOUD_COMPUTER_NODEMON volume and take ownership of it
 yarn --cwd ../docker docker run \
@@ -48,9 +36,7 @@ yarn --cwd ../docker docker run \
   --user root \
   --volume $CLOUD_COMPUTER_NODEMON_VOLUME:$CLOUD_COMPUTER_NODEMON \
   $CLOUD_COMPUTER_REGISTRY/$CLOUD_COMPUTER_IMAGE:latest \
-  chown -R cloud:cloud $CLOUD_COMPUTER_NODEMON
-
-echo 7777777777777777777777777777777777777
+  chown -R cloud:cloud $CLOUD_COMPUTER_NODEMON &
 
 # Create the CLOUD_COMPUTER_TMUX volume and take ownership of it
 yarn --cwd ../docker docker run \
@@ -58,9 +44,7 @@ yarn --cwd ../docker docker run \
   --user root \
   --volume $CLOUD_COMPUTER_TMUX_VOLUME:$CLOUD_COMPUTER_TMUX \
   $CLOUD_COMPUTER_REGISTRY/$CLOUD_COMPUTER_IMAGE:latest \
-  chown -R cloud:cloud $CLOUD_COMPUTER_TMUX
-
-echo 8888888888888888888888888888888888888
+  chown -R cloud:cloud $CLOUD_COMPUTER_TMUX &
 
 # Create the CLOUD_COMPUTER_X11 volume and take ownership of it
 yarn --cwd ../docker docker run \
@@ -68,19 +52,13 @@ yarn --cwd ../docker docker run \
   --user root \
   --volume $CLOUD_COMPUTER_X11_VOLUME:$CLOUD_COMPUTER_X11 \
   $CLOUD_COMPUTER_REGISTRY/$CLOUD_COMPUTER_IMAGE:latest \
-  chown -R cloud:cloud $CLOUD_COMPUTER_X11
-
-echo 9999999999999999999999999999999999999
+  chown -R cloud:cloud $CLOUD_COMPUTER_X11 &
 
 # Wait for bootstraps to complete in parallel
-# wait
+wait
 
 # Clone cloud computer repository into docker volume
 yarn --cwd ../git clone
 
-echo XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-
 # Bootstrap terraform configuration
 yarn --cwd ../terraform bootstrap
-
-echo YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY
