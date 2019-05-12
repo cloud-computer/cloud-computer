d619ce3fa4839ad65d4cf47166a15cf1d37e7b03
Jackson Delahunt <jackson@stemn.com>
2019-05-12 19:23:51 +1000

    no-exec

--- infrastructure/cloud-computer/scripts/start.sh
+++ infrastructure/cloud-computer/scripts/start.sh
@@ -18,8 +18,8 @@ echo ZZZZZZZ PRE
 echo ZZZZZZZ PRE
 echo ZZZZZZZ PRE
 
-yarn exec:terminal yarn --cwd infrastructure/tmux gotty:start
-yarn exec:terminal zsh -c 'echo $HOSTNAME'
+# yarn exec:terminal yarn --cwd infrastructure/tmux gotty:start
+# yarn exec:terminal zsh -c 'echo $HOSTNAME'
 
 echo XXXXXXX DONE
 echo XXXXXXX DONE
