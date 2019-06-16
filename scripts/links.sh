98b45c96324a94bd703540e7835818e9f3990036
Jackson Delahunt <jackson@stemn.com>
2019-06-16 22:09:11 +1000

    links

--- scripts/links.sh
+++ scripts/links.sh
@@ -1,8 +1,6 @@
 # Export cloud computer shell environment
 eval "$(yarn --cwd infrastructure/cloud-computer environment)"
 
-# Print links for click-to-open access
+# Print link for click-to-open access
 echo
-yarn apps | \
-  tr \  \\n | \
-  xargs -n 1 -I% echo https://%.$CLOUD_COMPUTER_HOST_DNS
+echo https://$CLOUD_COMPUTER_HOST_DNS
