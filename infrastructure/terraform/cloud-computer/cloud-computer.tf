65af3ec84362558f518d593e4925408f9b0a7492
Jackson Delahunt <jackson@stemn.com>
2019-06-18 14:30:04 +1000

    env-creds

--- infrastructure/terraform/cloud-computer/cloud-computer.tf
+++ infrastructure/terraform/cloud-computer/cloud-computer.tf
@@ -81,7 +81,7 @@ resource "google_compute_instance" "cloud-computer" {
   }
 
   metadata {
-    ssh-keys = "core:${tls_private_key.cloud-computer.public_key_openssh}"
+    ssh-keys = "ubuntu:${tls_private_key.cloud-computer.public_key_openssh}"
   }
 
   network_interface {
@@ -97,7 +97,7 @@ resource "google_compute_instance" "cloud-computer" {
       agent = false
       private_key = "${tls_private_key.cloud-computer.private_key_pem}"
       type = "ssh"
-      user = "core"
+      user = "ubuntu"
     }
 
     inline = [
