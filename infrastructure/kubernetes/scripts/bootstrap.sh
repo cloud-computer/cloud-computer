a1b24ca12da28ea84a4e53ba89105ba8f7885d2b
Jackson Delahunt <jackson@stemn.com>
2019-06-24 23:29:15 +1000

    kube-bootstrap

--- infrastructure/kubernetes/scripts/bootstrap.sh
+++ infrastructure/kubernetes/scripts/bootstrap.sh
@@ -1,5 +1,14 @@
+# Export cloud computer shell environment
+eval "$(yarn --cwd ../cloud-computer environment)"
+
 # Create a kind kubernetes cluster
-yarn kind create cluster \
+yarn --cwd ../docker docker run \
+  --rm \
+  --volume $CLOUD_COMPUTER_KUBERNETES_VOLUME:$CLOUD_COMPUTER_KUBERNETES \
+  --volume $CLOUD_COMPUTER_REPOSITORY_VOLUME:$CLOUD_COMPUTER_REPOSITORY \
+  --workdir $CLOUD_COMPUTER_REPOSITORY \
+  $CLOUD_COMPUTER_IMAGE \
+  kind create cluster \
   --name cloud-computer
 
 # Bootstrap helm
@@ -12,3 +21,15 @@ yarn --cwd ../helm helm install \
 
 # Start compose etcd
 yarn kubectl apply -f kind-compose-etcd.yaml
+
+# Start compose-on-kubernetes
+yarn --cwd ../docker docker run \
+  --rm \
+  --volume $CLOUD_COMPUTER_KUBERNETES_VOLUME:$CLOUD_COMPUTER_KUBERNETES \
+  --volume $CLOUD_COMPUTER_REPOSITORY_VOLUME:$CLOUD_COMPUTER_REPOSITORY \
+  --workdir $CLOUD_COMPUTER_REPOSITORY \
+  $CLOUD_COMPUTER_IMAGE \
+  compose-on-kubernetes \
+  -namespace=compose \
+  -etcd-servers=http://compose-etcd-client:2379 \
+  -tag=v0.4.18
