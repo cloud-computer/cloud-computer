8dac2a76e2f88c5ea15763fe0b5b5ef1f9d0f831
Jackson Delahunt <jackson@stemn.com>
2019-06-25 16:13:37 +1000

    istio

--- infrastructure/kubernetes/scripts/bootstrap.sh
+++ infrastructure/kubernetes/scripts/bootstrap.sh
@@ -22,3 +22,18 @@ yarn compose-on-kubernetes \
   -namespace=compose \
   -etcd-servers=http://compose-etcd-client:2379 \
   -tag=v0.4.18
+
+# Create the istio-system namespace
+yarn kubectl create namespace istio-system
+
+# Generate istio install template
+yarn --cwd ../helm helm template install/kubernetes/helm/istio \
+  --name istio \ 
+  --set global.mtls.enabled=false \
+  --set tracing.enabled=true \ 
+  --set kiali.enabled=true \
+  --set grafana.enabled=true \
+  --namespace istio-system > $CLOUD_COMPUTER_KUBERNETES/istio.yaml
+
+# Install istio
+yarn kubectl apply -f $CLOUD_COMPUTER_KUBERNETES/istio.yaml
