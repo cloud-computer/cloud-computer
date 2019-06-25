c7b5cc735a88c1d4663e841f1e8fa2303a2cebf4
Jackson Delahunt <jackson@stemn.com>
2019-06-25 16:16:40 +1000

    helm

--- infrastructure/helm/scripts/helm.sh
+++ infrastructure/helm/scripts/helm.sh
@@ -2,6 +2,7 @@
 eval "$(yarn --cwd ../cloud-computer environment)"
 
 yarn --cwd ../docker docker run \
+  --env HELM_HOME=$CLOUD_COMPUTER_KUBERNETES/helm \
   --rm \
   --volume $CLOUD_COMPUTER_KUBERNETES_VOLUME:$CLOUD_COMPUTER_KUBERNETES \
   --volume $CLOUD_COMPUTER_REPOSITORY_VOLUME:$CLOUD_COMPUTER_REPOSITORY \
