560ce2c4b4849e477fd0b5871cbc1af7b26bb585
Jackson <jackson@stemn.com>
2019-07-18 14:00:56 +1000

    fix: remove yarn

--- apps/webapp/worker-service/lib/scripts/run.sh
+++ apps/webapp/worker-service/lib/scripts/run.sh
@@ -2,4 +2,6 @@
 
 printf "\033[1;31mProvisioning...\033[0m\n"
 cd /cloud-computer/cloud-computer
-yarn CLOUD_COMPUTER_HOST_ID=$1 yarn create:cloud-computer
+pwd
+echo "building $1"
+CLOUD_COMPUTER_HOST_ID=$1 yarn create:cloud-computer
