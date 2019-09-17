dfdee06795eb365408267fabe54cad60151f8ff5
Jackson Delahunt <jackson@stemn.com>
2019-09-17 16:44:08 +1000

    fix

--- apps/webapp/scripts/start.sh
+++ apps/webapp/scripts/start.sh
@@ -4,6 +4,9 @@ eval "$(yarn --cwd ../../infrastructure/cloud-computer environment)"
 # Export webapp shell environment
 eval "$(yarn environment)"
 
+# Install dependencies
+yarn
+
 # Build the development containers
 docker-compose build
 
